1e123d612d1d9a27badb4af184d8c958b77d27e1
fengxiaobin <rgfxb@qq.com>
2018-12-07 22:09:53 +0800

    find num in a sorted array

--- ch6/GetNumberOfK.cpp
+++ ch6/GetNumberOfK.cpp
@@ -4,13 +4,13 @@ using namespace std;
 /**
 ** find the number of k in a sorted array
 **/
-int GetFirstK(int* data,int length, int k,int start, int high)
+int GetFirstK(int* data,int length, int k,int start, int end)
 {
-    if(high-start)
+    if(end<start)
     {
         return -1;
     }
-    int midIndex = (high -low)/2+low;
+    int midIndex = (end - start)/2+start;
     int midNum = data[midIndex];
     if( midNum == k)
     {
@@ -19,6 +19,10 @@ int GetFirstK(int* data,int length, int k,int start, int high)
             // 确定midIndex是第一个k的下标  递归终止的条件
             return midIndex;
         }
+		else
+		{
+			end = midIndex-1;
+		}
     }
     else if(midNum<k)
     {
@@ -26,27 +30,32 @@ int GetFirstK(int* data,int length, int k,int start, int high)
     }
     else
     {
-        high = midIndex - 1;
+        end = midIndex - 1;
     }
-    return GetFirstK(data,k,start,end);
+    return GetFirstK(data,length,k,start,end);
     
 }
 
-int GetLastK(int* data, int length, int k;int start,int high)
+int GetLastK(int* data, int length, int k,int start,int end)
 {
-     if(high-start)
+    if(end<start)
     {
         return -1;
     }
-    int midIndex = (high -low)/2+low;
+    int midIndex = (end - start)/2+start;
     int midNum = data[midIndex];
     if( midNum == k)
     {
-        if((data[midIndex+1]!=k&&midIndex<length-1)||midIndex==length-1)
+        if((data[midIndex+1]!=k&&midIndex<length-1)
+				||midIndex==length-1)
         {
-            // 确定midIndex是最后一个k的下标  递归终止的条件
+		   // 确定midIndex是最后一个k的下标  递归终止的条件
             return midIndex;
         }
+		else
+		{
+			start = midIndex + 1;
+		}
     }
     else if(midNum<k)
     {
@@ -54,9 +63,9 @@ int GetLastK(int* data, int length, int k;int start,int high)
     }
     else
     {
-        high = midIndex - 1;
+        end = midIndex - 1;
     }
-    return GetFirstK(data,k,start,end);
+    return GetLastK(data,length,k,start,end);
     
 }
 
@@ -67,10 +76,11 @@ int GetNumberOfK(int *data,int length, int k)
         return -1;
     }
     int i = GetFirstK(data,length,k,0,length-1);
-    int j = GetLastK(data,length,k,0,lenght-1);
-    if(i==-1||j==-1)
+    int j = GetLastK(data,length,k,0,length-1);
+   	cout<<i<<" "<<j<<endl;
+   	if(i==-1||j==-1)
         return -1;
-    return j-1+1;
+    return j-i+1;
 }
 
 int main()
@@ -82,6 +92,6 @@ int main()
     data[3]=3;
     data[4]=4;
     data[5]=5;
-    cout<<GetNumberOfK[data,6,3]<<endl;
+    cout<<GetNumberOfK(data,6,3)<<endl;
     return 0;
 }
